fef8e2e6af35fd46bfefd6732d4268a9b01655a6
Hamidreza Validi <hamidreza.validi2@gmail.com>
2022-08-04 15:04:54 -0500

    Update Main.py

--- src/Main.py
+++ src/Main.py
@@ -95,7 +95,6 @@ def export_to_png(m, assignment, filename):
     my_fig.set_size_inches(my_fig.get_size_inches() * RESIZE_FACTOR)
     plt.axis('off')
     my_fig.savefig(filename)
-    plt.close()
     
 # prepare csv file by writing column headers
 with open(results_filename,'w', newline = '') as csvfile:   
